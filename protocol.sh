24ab6fb9658f005b745d1a46f879380d740dca5a
Christian <chris.arevir@gmail.com>
2019-04-25 10:12:45 -0700

    WIP, testing download after this

---
+++ protocol.sh
@@ -0,0 +1,14 @@
+
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if type -p curl || [[ -z curl ]]; then
+  echo "Curl exists!"
+fi
